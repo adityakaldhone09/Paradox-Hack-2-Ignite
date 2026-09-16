da603ec55811389ab5f7126d1459e3b911938f49
Aditya Kaldhone <adityakaldhone9@gmail.com>
2026-09-16 11:50:42 +0530

    Final Setup

--- artifacts/api-server/dist/pino-worker.mjs
+++ artifacts/api-server/dist/pino-worker.mjs
@@ -4122,7 +4122,7 @@ var require_pino = __commonJS({
     function pinoBundlerAbsolutePath(p) {
       try {
         const path = __require("path");
-        const outputDir = "/Users/adityak/Projects/Paradox-Hack2Ignite/ExamChain-Security-Platform/artifacts/api-server/dist";
+        const outputDir = "/Users/adityak/Projects/Paradox-Hack-2-Ignite/artifacts/api-server/dist";
         return path.resolve(outputDir, p.replace(/^\.\//, ""));
       } catch (e) {
         const f = new Function("p", "return new URL(p, import.meta.url).pathname");
